81313660c181815ad7c6b1a5d0ffccfade988344
jrodriguez86 <57810392+jrodriguez86@users.noreply.github.com>
2020-04-01 22:33:43 -0400

    Create Form complete and saving to server

--- covid_19/src/App.js
+++ covid_19/src/App.js
@@ -1,28 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NewForm from './components/NewForm.js'
 
-class App extends React.Component () {
+// .env BaseURL for React
+let baseURL = process.env.REACT_APP_BASEURL
+
+
+if (process.env.NODE_ENV === 'development') {
+  baseURL = 'http://localhost:9000'
+} else {
+  baseURL = 'https://fathomless-sierra-68956.herokuapp.com'
+}
+
+console.log('current base URL:', baseURL)
+
+fetch(baseURL+ '/covidstats')
+  .then(data => {
+    return data.json()},
+    err => console.log(err))
+  .then(parsedData => console.log(parsedData),
+   err => console.log(err))
+
+
+
+class App extends React.Component {
+
+  state = {
+    requests: []
+  }
+
+  componentDidMount() {
+    this.getComments()
+  }
+
+  getComments = () => {
+    fetch(baseURL+ '/covidstats')
+      .then(data => {
+        return data.json()},
+        err => console.log(err))
+        .then(parsedData => this.setState({
+          requests: parsedData
+        }),
+         err=> console.log(err))
+  }
+
+ // New Form HandleAdd 
+  handleAddRequest = (requests) => {
+    const copyRequest = [...this.state.requests]
+    copyRequest.unshift(requests)
+    this.setState({
+      requests: copyRequest,
+      name: '',
+      comments: '',
+      location: '',
+    })
+  }
+
+  // handleSubmit = (event) => {
+  //   event.preventDefault()
+  //   fetch(this.props.baseURL + '/covidstats', {
+  //     method: 'POST',
+  //     body: JSON.stringify({requests: this.state.requests}),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   }).then (res => res.json())
+  //     .then (resJson => {
+  //       this.props.handleAddRequest(resJson)
+  //       this.setState({
+  //         requests: ''
+  //       })
+  //   }).catch (error => console.error({'Error': error}))
+  // }
+
   render() {
+    console.log(this.state.requests)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    // Comments/Requests
+    <div className="commentsContainer">
+      <h1 className="comment-title">Post any comments or requests in your area</h1>
+      <NewForm baseURL={baseURL}
+  handleAddRequest={this.handleAddRequest}/>
     </div>
   );
 }
 }
+
 
 export default App;
