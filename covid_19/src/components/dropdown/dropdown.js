3ead442fc74c68ad9b0898e21af5f0c07fba7d20
dejavxtrem <dejavxtrem@hotmail.com>
2020-04-04 16:51:40 -0500

    frontend function

--- covid_19/src/components/dropdown/dropdown.js
+++ covid_19/src/components/dropdown/dropdown.js
@@ -5,16 +5,57 @@ import  './dropdown.css'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+//drop down countries imports
+import countryFinder from './dropdowncountries';
+
+let apiKEY = '39f4998951msh07883f04b2178e7p1b36dbjsnbf1a0ddc7ca0'
 
 
 class DropDown extends React.Component {
+
+  state = {
+    covidSearch: {countries_stats: []}
+
+  }
+
+  componentDidMount() {
+    this.getCountry();
+    this.onSelectFlag();
+  }
+
+  //API call to get search data
+  getCountry = () => {
+    fetch(`https://coronavirus-monitor.p.rapidapi.com/coronavirus/cases_by_country.php`, {
+      "method": "GET",
+      headers: {
+       'x-rapidapi-host': 'coronavirus-monitor.p.rapidapi.com',
+       'x-rapidapi-key': `${apiKEY}`
+ 
+      }
+    }).then(data => data.json(), err => console.log(err))
+      .then(parsedData => this.setState({covidSearch: parsedData}), err => console.log('parsedData', err))
+  }
+
+
+  //Convert DropDown country abriv to search country string
+  onSelectFlag = (country) => {
+      for (let [key, value] of Object.entries(countryFinder)) {
+         if (country === key) {
+           console.log(`${value}`)
+         }
+      }
+      
+  }
+
     render () {
+
+      console.log(this.state.covidSearch)
         return (
             <Container className="dropdown">
             {/* dropdown component on col */}
             <Row>
               <Col className="coldrop">
-              <ReactFlagsSelect  className="menu-flags"/>
+              <ReactFlagsSelect  className="menu-flags" onSelect={this.onSelectFlag}/>
               </Col>
             </Row>
           </Container>
--- covid_19/src/components/table/table.js
+++ covid_19/src/components/table/table.js
@@ -15,8 +15,8 @@ class TableComponent extends React.Component {
     //   }
 
     render () {
-    console.log(this.props.covidApiData.countries_stat)
-       
+    //console.log(this.props.covidApiData.countries_stat[1])
+
         return (
             <Table striped bordered hover responsive="lg">
             <thead>
@@ -29,68 +29,68 @@ class TableComponent extends React.Component {
             </thead>
                 <tbody>
                     <tr>
-                    {/* <td>{this.getData()}</td> */}
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>{this.props.covidApiData.countries_stat[0].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[0].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[0].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[0].total_recovered}</td>
                     </tr>
                     <tr>
-                    <td>2</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[1].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[1].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[1].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[1].total_recovered}</td>
                     </tr>
                     <tr>
-                    <td>3</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[2].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[2].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[2].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[2].total_recovered}</td>
                     </tr>
                     <tr>
-                    <td>4</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[3].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[3].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[3].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[3].total_recovered}</td>
                     </tr>
                     <tr>
-                    <td>5</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[4].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[4].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[4].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[4].total_recovered}</td>
                     </tr>
                     <tr>
-                    <td>6</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-
-                    <tr>
-                    <td>7</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[5].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[5].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[5].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[5].total_recovered}</td>
                     </tr>
 
                     <tr>
-                    <td>8</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[6].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[6].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[6].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[6].total_recovered}</td>
                     </tr>
 
                     <tr>
-                    <td>9</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[7].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[7].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[7].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[7].total_recovered}</td>
                     </tr>
 
                     <tr>
-                    <td>10</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>{this.props.covidApiData.countries_stat[8].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[8].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[8].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[8].total_recovered}</td>
+                    </tr>
+
+                    <tr>
+                    <td>{this.props.covidApiData.countries_stat[9].country_name}</td>
+                    <td>{this.props.covidApiData.countries_stat[9].cases}</td>
+                    <td>{this.props.covidApiData.countries_stat[9].deaths}</td>
+                    <td>{this.props.covidApiData.countries_stat[9].total_recovered}</td>
                     </tr>
                     
                 </tbody>
